91e1c42aba624bea2d26b98da839abbe561f8247
fndigrazia <digrazia.nicolas@gmail.com>
2024-01-16 19:33:39 -0300

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,28 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('express')();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware para parsear el cuerpo de la solicitud como JSON
-app.use(bodyParser.json());
-
-// Ruta para manejar solicitudes POST
-app.post('/api/endpoint', (req, res) => {
-  // Obtiene los datos de la solicitud POST
-  const data = req.body;
-
-  // Construye la respuesta en formato JSON
-  const response = {
-    message: 'Solicitud POST recibida con éxito',
-    data: data
-  };
-
-  // Envía la respuesta en formato JSON
-  res.json(response);
-});
-
-// Inicia el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
+app.get('/api', (req, res) => {
+  res.send({'hola':'hello'})
+})
+module.exports = app
